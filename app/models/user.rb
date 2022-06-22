04bf8545eb42da6a8cb2ce6f9990cac403669649
tosinori-m <your.email@example.com>
2022-06-22 16:29:12 +0900

    Add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :messages
+  has_many :likes
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
+  resources :messages, only: ['index'] do
+    member do
+      resources :likes, only: ['create']
+    end
+  end
+  resources :likes, only: ['destroy']
 end
